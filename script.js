a2a8a77dc28a1832ebab8a38c638c77bf69471b5
Jovan Sreco <jovan.sreco@gmail.com>
2022-07-23 13:31:10 +0200

    Setup

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         data.results.forEach( property => {
+//         console.log([...property])
+//        })
+
+
+//     })
+
